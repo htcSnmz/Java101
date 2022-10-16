4a40466b55c2cc2f2a9772340bd6205e4f51406c
Hatice SÖNMEZ <htcsonmez428@gmail.com>
2022-10-16 21:36:59 +0300

    comment line added

--- Odev36.java
+++ Odev36.java
@@ -1,3 +1,6 @@
+//Girilen sayı 0 veya negatif olduğu yere kadar girilen sayıdan 5 rakamını çıkarın. Her çıkarma işlemi sırasında ekrana son değeri yazdırın. 
+//Sayı negatif veya 0 olduktan sonra tekrar 5 ekleyin. Yine her ekleme işleminde sayının son değerini ekrana yazdırın.
+
 package com.company;
 
 import java.util.Scanner;
