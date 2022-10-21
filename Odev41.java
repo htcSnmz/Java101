8ecdffb8c18ec58f33400b3558ce6f154d1639c2
Hatice SÖNMEZ <htcsonmez428@gmail.com>
2022-10-21 22:41:49 +0300

    hw41 added

---
+++ Odev41.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Odev41 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] arr = {15, 12, 788, 1, -1, -778, 2, 24, -32, 54};
+        int min = arr[0];
+        int max = arr[0];
+
+        System.out.println(Arrays.toString(arr));
+        System.out.print("Sayı Giriniz : ");
+        int input = scan.nextInt();
+
+        Arrays.sort(arr);
+        for (int i : arr) {
+            if (i < input) {
+                min = i;
+            }
+            if (i > input) {
+                max = i;
+                break;
+            }
+        }
+        System.out.println("Girilen sayı : " + input);
+        System.out.println("Girilen sayıdan küçük en yakın sayı : " + min);
+        System.out.println("Girilen sayıdan büyük en yakın sayı : " + max);
+    }
+}
